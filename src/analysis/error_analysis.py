3e95e2058a4c7f7822bdad49837d621ec82fca89
gall <daniel.gallagher.js@gmail.com>
2026-02-03 12:36:40 +0100

    added correlation tests

--- src/analysis/error_analysis.py
+++ src/analysis/error_analysis.py
@@ -1,88 +1,241 @@
-import pandas as pd
-from pathlib import Path
-from transliterate import translit
-from deep_translator import GoogleTranslator
 from scipy.stats import pearsonr, spearmanr
 from scipy.special import logsumexp
-import asyncio
+from transformers import AutoTokenizer
+from transliterate import translit
+import pandas as pd
 import numpy as np
+from pathlib import Path
+import os
 
-async def translate_text(text, src="ka", dest="en"):
-    return GoogleTranslator(source=src, target=dest).translate(text)
+# All models used in the evaluation and their Hugging Face repos.
+models = {
+    "HPLT": "HPLT/hplt_bert_base_ka", 
+    "RemBERT": "google/rembert",
+    "mBERT": "google-bert/bert-base-multilingual-cased", 
+    "XLM-RoBERTa(bs)": "FacebookAI/xlm-roberta-base",
+    "XLM-RoBERTa(lg)": "FacebookAI/xlm-roberta-large",
+    "mGPT-1.3B-Georgian": "ai-forever/mGPT-1.3B-georgian",
+    "GPT2-GEO": "Kuduxaaa/gpt2-geo",
+}
 
-async def translate_column(data, column_name, result_column_name):
-    translated_values = []
-    for text in data[column_name]:
-        if isinstance(text, str):
-            try:
-                translated_text = await translate_text(text)
-                translated_values.append(translated_text)
-            except:
-                translated_values.append("X")
-        else:
-            translated_values.append(text)
-    data[result_column_name] = translated_values
+# Which datasets test which grammatical case i.e. nominative, dative, or ergative.
+config_splits = {
+    "NOM": ["INTR-S1-NOM-word-level","S1-NOM-word-level","S2-NOM-word-level","S3-NOM-word-level"],
+    "DAT": ["S1-DAT-word-level","S3-DAT-word-level"],
+    "ERG": ["S2-ERG-word-level"]
+}
+
+# Individual dataset configuration e.g. which cases are grammatical or ungrammatical.
+dataset_configs = {
+    "INTR-S1-NOM-word-level": {
+        "task": "intransitive-nom-subj",
+        "grammatical_col": "nom",
+        "ungrammatical_cols": [
+            "dat",
+            "erg"
+        ]
+    },
+    "S1-NOM-word-level": {
+        "task": "transitive-nom-dat-subj",
+        "grammatical_col": "nom",
+        "ungrammatical_cols": [
+            "dat",
+            "erg"
+        ]
+    },
+    "S1-DAT-word-level": {
+        "task": "transitive-nom-dat-obj",
+        "grammatical_col": "dat",
+        "ungrammatical_cols": [
+            "nom",
+            "erg"
+        ]
+    },
+    "S2-ERG-word-level": {
+        "task": "transitive-erg-nom-subj",
+        "grammatical_col": "erg",
+        "ungrammatical_cols": [
+            "nom",
+            "dat"
+        ]
+    },
+    "S2-NOM-word-level": {
+        "task": "transitive-erg-nom-obj",
+        "grammatical_col": "nom",
+        "ungrammatical_cols": [
+            "erg",
+            "dat"
+        ]
+    },
+    "S3-DAT-word-level": {
+        "task": "transitive-dat-nom-subj",
+        "grammatical_col": "dat",
+        "ungrammatical_cols": [
+            "nom",
+            "erg"
+        ]
+    },
+    "S3-NOM-word-level": {
+        "task": "transitive-dat-nom-obj",
+        "grammatical_col": "nom",
+        "ungrammatical_cols": [
+            "dat",
+            "erg"
+        ]
+    },
+}
+
+# Computes the number of tokens for a model in the grammatical feature as well as the full sentence.
+def num_tokens(data, model: str, grammatical_feature:str):
+    tokeniser = AutoTokenizer.from_pretrained(model)
+    def tokenise(t: str):
+        return tokeniser.tokenize(t)
+    data['word_token_length'] = data[f'form_grammatical_{grammatical_feature}'].apply(tokenise)
+    data['word_token_length'] = data['word_token_length'].apply(len)
+
+    data['sent_token_length'] = data['masked_text'].apply(tokenise)
+    data['sent_token_length'] = data['sent_token_length'].apply(len)
     return data
 
-def analyse_sentences(file_path, sort_by='certainty', ascending=True):
-    data = pd.read_csv(file_path)
-    # data = data.iloc[:3,]
+def analyse_sentences(data, grammatical_feature:str, evaluation_type, model):
+    ungrammatical_features = ["nom","erg","dat"]
+    ungrammatical_features.remove(grammatical_feature)
     
     eps = 1e-12  # prevents log(0)
 
-    logp_erg = np.log(data["p_form_grammatical_erg"] + eps)
-    logp_nom = np.log(data["p_form_ungrammatical_nom"] + eps)
-    logp_dat = np.log(data["p_form_ungrammatical_dat"] + eps)
+    data = num_tokens(data, model, grammatical_feature)
 
-    data["log_odds"] = logp_erg - logsumexp(
-        np.vstack([logp_nom, logp_dat]).T,
+    if evaluation_type == "word-level":
+        logp_g = np.log(data[f"p_form_grammatical_{grammatical_feature}"] + eps)
+        logp_ug1 = np.log(data[f"p_form_ungrammatical_{ungrammatical_features[0]}"] + eps)
+        logp_ug2 = np.log(data[f"p_form_ungrammatical_{ungrammatical_features[1]}"] + eps)
+    else:
+        logp_g = data[f"I_form_grammatical_{grammatical_feature}"]
+        logp_ug1 = data[f"I_form_ungrammatical_{ungrammatical_features[0]}"]
+        logp_ug2 = data[f"I_form_ungrammatical_{ungrammatical_features[1]}"]
+
+    data["log_odds"] = logp_g - logsumexp(
+        np.vstack([logp_ug1, logp_ug2]).T,
         axis=1
     )
+    data[f"log_odds_{grammatical_feature}_{ungrammatical_features[0]}"] = logp_g - logp_ug1
+    data[f"log_odds_{grammatical_feature}_{ungrammatical_features[1]}"] = logp_g - logp_ug2
 
-    data["word_length"] = data["form_grammatical_erg"].apply(lambda word: len(word))
-    data["sent_length"] = data["masked_text"].apply(lambda sent: len(sent))
+    pref_ug1 = sum(data[f"p_form_ungrammatical_{ungrammatical_features[0]}"] > data[f"p_form_ungrammatical_{ungrammatical_features[1]}"])/len(data)
 
-    pearson_corr_word, _ = pearsonr(data["word_length"],data["log_odds"])
-    spearman_corr_word, _ = spearmanr(data["word_length"],data["log_odds"])
+    pears_wl, pears_wl_pval = pearsonr(data["word_token_length"],data["log_odds"])
+    pears_wl_char, pears_wl_pval_char = pearsonr(data["word_length"],data["log_odds"])
+    spearm_wl_char, spearm_wl_char_pval = spearmanr(data["word_token_length"],data["log_odds"])
 
-    pearson_corr_sent, _ = pearsonr(data["sent_length"],data["log_odds"])
-    spearman_corr_sent, _ = spearmanr(data["sent_length"],data["log_odds"])
+    pears_wl_erg_nom, _ = pearsonr(data["word_token_length"],data["log_odds_erg_nom"])
+    spearm_wl_erg_nom, _ = spearmanr(data["word_token_length"],data["log_odds_erg_nom"])
 
-    data['translit'] = data['form_grammatical_erg'].apply(
+    pears_wl_erg_dat, _ = pearsonr(data["word_token_length"],data["log_odds_erg_dat"])
+    spearm_wl_erg_dat, _ = spearmanr(data["word_token_length"],data["log_odds_erg_dat"])
+
+    pears_sent, pears_sent_pval = pearsonr(data["sent_token_length"],data["log_odds"])
+    spearm_sent, spearm_sent_pval = spearmanr(data["sent_token_length"],data["log_odds"])
+
+    data['translit'] = data[f'form_grammatical_{grammatical_feature}'].apply(
         lambda x: translit(x, 'ka', reversed=True) if isinstance(x, str) else x
     )
 
-    sorted_data = data.sort_values(by=sort_by, ascending=ascending)
+    return data, {
+            "pears_wl": pears_wl,
+            "pears_wl_pval": pears_wl_pval,
+            "pears_wl_char": pears_wl_char,
+            "pears_wl_pval_char": pears_wl_pval_char,
+            f"pref_{ungrammatical_features[0]}": pref_ug1,
+            "spearm_wl_char": spearm_wl_char,
+            "spearm_wl_char_pval": spearm_wl_char_pval,
+            "pears_wl_erg_nom": pears_wl_erg_nom,
+            "spearm_wl_erg_nom": spearm_wl_erg_nom,
+            "pears_wl_erg_dat": pears_wl_erg_dat,
+            "spearm_wl_erg_dat": spearm_wl_erg_dat,
+            "pears_sent": pears_sent,
+            "pears_sent_pval": pears_sent_pval,
+            "spearm_sent": spearm_sent,
+            "spearm_sent_pval": spearm_sent_pval,
+    }
 
-    result = sorted_data[['sentence_id', 'masked_text', 'form_grammatical_erg',
-                           'form_ungrammatical_nom', 'form_ungrammatical_dat',
-                           'p_form_grammatical_erg', 'p_form_ungrammatical_nom',
-                           'p_form_ungrammatical_dat', 'certainty','log_odds', 'translit']]
 
-    return result, pearson_corr_word, spearman_corr_word, pearson_corr_sent, spearman_corr_sent
-
-async def main():
+def main():
     RESULTS_PATH = "./results/full"
-    file_path = Path(RESULTS_PATH) / 'mlm-mBERT-S2-ERG-word-level.csv'
-    result, pearson_corr_word, spearman_corr_word, pearson_corr_sent, spearman_corr_sent = analyse_sentences(
-            file_path, sort_by='log_odds', ascending=True
-    )
-    result = await translate_column(result, 'form_grammatical_erg','translated_form')
-    result = await translate_column(result, 'masked_text','translated_sent')
-    for i, row in result.iterrows():
-        print("====")
-        print("Form: ", row["form_grammatical_erg"])
-        print("Rough Translation (Word): ", row["translated_form"])
-        print("Rough Translation (Sent): ", row["translated_sent"])
-        print("Transliterated: ", row["translit"])
-        print("Pref for Erg: ", row["log_odds"])
-        print("====")
 
-    print([["form_grammatical_erg","log_odds","translated_form","translit","translated_sent"]])
-    print(f"Pearson r (Word): {pearson_corr_word}")
-    print(f"Spearman r (Word): {spearman_corr_word}")
-    print(f"Pearson r (Sent): {pearson_corr_sent}")
-    print(f"Spearman r (Sent): {spearman_corr_sent}")
+    task_results = {}
+
+    for focus in dataset_configs.keys():
+        task_results[focus] = []
+        grammatical_feature = dataset_configs[focus]["grammatical_col"]
+
+        matching_files = []
+        all_data = []
+        for filename in os.listdir(RESULTS_PATH):
+            if focus in filename:
+                matching_files.append(filename)
+        print("Matching files: ", matching_files)
+
+        for filename in matching_files:
+            full_path = Path(RESULTS_PATH) / filename
+            df = pd.read_csv(full_path)
+            all_data.append(df)
+
+        all_token_lengths = {}
+        for filename, data in zip(matching_files, all_data):
+            for key, value in models.items():
+                if key in filename:
+
+                    model = value
+                    print("Setting model to ", model)
+                    break
+
+            result, correlations = analyse_sentences(
+                    data,grammatical_feature,"word-level",model
+            )
+
+            print("====")
+            print("Filename: ", filename)
+            all_token_lengths[model] = result["word_token_length"].mean()
+            for key, value in correlations.items():
+                print(key, round(value,2))
+            print("\n\n\n")
+            for model, tl in all_token_lengths.items():
+                print(f"{model}: {tl}")
+            print("====")
+            print("\n\n\n")
+
+            task_results[focus].append(correlations)
+
+    for form,tasks in config_splits.items():
+        all_task_results = [task_results[t] for t in tasks]
+
+        all_correlations = []
+        for task_result in all_task_results:
+            for correlations in task_result:
+                print("Adding correlation:")
+                print(correlations)
+                print("\n\n")
+                all_correlations.append(correlations)
+                
+        full_df = pd.concat([pd.DataFrame([c]) for c in all_correlations],ignore_index=True)
+        print(full_df)
+
+        print("Processing...")
+        print(full_df.head())
+        print("Tasks: ", tasks)
+        print("\n\n")
+
+        print(f"===={form}====")
+        print("Dataset Size: ", len(full_df))
+        print("Results:\n")
+        for c in full_df.columns:
+            print(f"Avg {c}: ", full_df[c].mean())
+            print(f"StdDev {c}: ", full_df[c].std())
+
+        print(f"{form} Word: ({full_df["pears_wl"].mean()},p={full_df["pears_wl_pval"].mean()})")
+        print(f"{form} Sentence: ({full_df["pears_sent"].mean()},p={full_df["pears_sent_pval"].mean()})")
+        print("====")
+        print("\n\n")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
